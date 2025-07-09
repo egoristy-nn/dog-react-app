7a22a108905eb3b0dcdc7073078806e8706a6eaa
Egor Vetkin <vetkin.yegor@yandex.ru>
2025-07-09 17:51:40 +0300

    доработана пагинация и дизайн

--- src/pages/GalleryPage/GalleryPage.tsx
+++ src/pages/GalleryPage/GalleryPage.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import styles from './GalleryPage.module.css';
+import Pagination from '@mui/material/Pagination';
 
 const images = Array.from({ length: 100 }, (_, i) => ({
   id: i + 1,
@@ -9,25 +11,33 @@ const images = Array.from({ length: 100 }, (_, i) => ({
 const PAGE_SIZE = 10;
 
 const GalleryPage = () => {
+  const navigate = useNavigate();
   const { pageNumber } = useParams();
   const currentPage = parseInt(pageNumber || '1');
 
   const offset = (currentPage - 1) * PAGE_SIZE;
   const paginatedImages = images.slice(offset, offset + PAGE_SIZE);
 
+  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    navigate(`/gallery/${value}`);
+  };
+
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+    <>
+    <div className={styles.container}>
       {paginatedImages.map((image) => (
-        <div key={image.id} style={{ margin: '10px' }}>
+        <div key={image.id} className={styles.card}>
           <img src={image.url} alt={`Image ${image.id}`} width="200" height="300" />
         </div>
       ))}
-      
-      <nav style={{ textAlign: 'center', marginTop: '20px' }}>
-        <Link to={`/gallery/${currentPage > 1 ? currentPage - 1 : 1}`}>Назад</Link>  
-        <Link to={`/gallery/${Math.min(currentPage + 1, Math.ceil(images.length / PAGE_SIZE))}`}>Вперед</Link>
-      </nav>
     </div>
+    <nav style={{ display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+        {/* <Link to={`/gallery/${currentPage > 1 ? currentPage - 1 : 1}`}>Назад</Link>  
+        <Link to={`/gallery/${Math.min(currentPage + 1, Math.ceil(images.length / PAGE_SIZE))}`}>Вперед</Link> */}
+        <Pagination count={10} page={currentPage} onChange={handlePageChange}/>
+    </nav>
+    </>
+    
   );
 };
 
